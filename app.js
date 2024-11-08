e34617025769970d921f199a6c4f046195568ecc
dareijan <tarpamama@protonmail.com>
2024-11-08 23:59:37 +0200

    Päivitys ensivaiheessa

--- app.js
+++ app.js
@@ -17,4 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var marker2 = L.marker([62.25677227963676, 25.696428305644275]).addTo(map);
     marker2.bindPopup("<b>Laajis seikkailupuosto</b> <br /> Koko perheen seikkailumaa<br />09:45-18:00 ks. kotisivut <br><a href=https://laajis.fi/>Laajis</a>");
 
+
+    var marker3 = L.marker([62.253328657379555, 25.830440248724543]).addTo(map);
+    marker3.bindPopup("<b>Asmalammen uimaranta</b> <br /> Matala, nopeasti syvenevä ranta. Laituri. Parkkipaikka vieressä. Roskikset. Kaksi uimakoppia. WC-kopit, oma paperi mukaan. </br><br /> Ei tulentekomahdollisuutta. Useita koiranuittopaikkoja, kumiveneen laskupaikkoja ja onkipaikkoja. <br /><br />Hiekkaranta avautuu etelään-lounaaseen. Vierestä kulkee ulkoilupolku. Bussipysäkki heti Asmalammintiellä josta kulkee useita vuoroja keskustaan ja Vaajakoskelle, bussi nro 3.<br /><br />Vedenlaatu mitataan kesäisin ja ollut viime vuosina hyvä.");
   });
