3f62f400bca7a7f84e094469fe8d2df517a0ded8
dareijan <tarpamama@protonmail.com>
2024-11-26 01:16:14 +0200

    Tietolahteen parannus

--- app.js
+++ app.js
@@ -12,34 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }).addTo(map);
 
 
-var csvdata = [
-	[
-    62.253328657379555,
-    25.830440248724543,
-    "Asmalammen uimaranta",
-    "Matala, nopeasti syvenevä ranta. Laituri. Parkkipaikka vieressä. Roskikset. Kaksi uimakoppia. WC-kopit, oma paperi mukaan.",
-    "Ei tulentekomahdollisuutta. Useita koiranuittopaikkoja, kumiveneen laskupaikkoja ja onkipaikkoja. Hiekkaranta avautuu etelään-lounaaseen. Vierestä kulkee ulkoilupolku.Vedenlaatu mitataan kesäisin ja ollut viime vuosina hyvä", 
-    "Bussipysäkki heti Asmalammintiellä josta kulkee useita vuoroja keskustaan ja Vaajakoskelle, bussi nro 3"
-  ],
-  [ 
-    62.25677227963676,
-    25.696428305644275,
-    "Laajis seikkailupuisto",
-    "Koko perheen seikkailumaa",
-    "09:45-18:00 ks. kotisivut ",
-    "<a href=https://laajis.fi/>Laajis</a>"],
-    [ 
-      62.259,
-      25.698,
-      "Laajavuoren kylpylä",
-      "Ma–to klo 8.00–21.00",
-      "Pe–la klo 8.00–22.00",
-      "Su klo 8.00–20.00",
-      "Pikkulasten allas. Aikuisten altaassa matalia kohtia. Vesihierontapisteitä. Erilaisia saunoja.",
-      "Allasosasto sulkeutuu 30 min ennen kylpylän sulkemista.",
-      "<a href=https://www.scandichotels.fi/hotellit/suomi/jyvaskyla/scandic-laajavuori/kylpyla>Laajavuoren kylpylä</a>"]    
-]
-
 var indeksi = document.getElementById("indeksi");
 
 function createindeksiElement(layer, otsikko){
@@ -61,8 +33,8 @@ function zoomaaMarkkeriin(e){
 
 var featuremappi = L.featureGroup().addTo(map);
 
-for (let i = 0; i < csvdata.length; i++) {  
-  rivi = csvdata[i];
+for (let i = 0; i < paikatdata.length; i++) {  
+  rivi = paikatdata[i];
 	var marker = L.marker([rivi[0], rivi[1]]).addTo(featuremappi);
   if (rivi.length == 4) {
     marker.bindPopup("<b>"+[rivi[2]+"</b>",rivi[3]].join("<br><br>"));
---
+++ paikat.js
@@ -0,0 +1,28 @@
+
+var paikatdata = [
+    [
+    62.253328657379555,
+    25.830440248724543,
+    "Asmalammen uimaranta",
+    "Matala, nopeasti syvenevä ranta. Laituri. Parkkipaikka vieressä. Roskikset. Kaksi uimakoppia. WC-kopit, oma paperi mukaan.",
+    "Ei tulentekomahdollisuutta. Useita koiranuittopaikkoja, kumiveneen laskupaikkoja ja onkipaikkoja. Hiekkaranta avautuu etelään-lounaaseen. Vierestä kulkee ulkoilupolku.Vedenlaatu mitataan kesäisin ja ollut viime vuosina hyvä", 
+    "Bussipysäkki heti Asmalammintiellä josta kulkee useita vuoroja keskustaan ja Vaajakoskelle, bussi nro 3"
+  ],
+  [ 
+    62.25677227963676,
+    25.696428305644275,
+    "Laajis seikkailupuisto",
+    "Koko perheen seikkailumaa",
+    "09:45-18:00 ks. kotisivut ",
+    "<a href=https://laajis.fi/>Laajis</a>"],
+  [ 
+      62.259,
+      25.698,
+      "Laajavuoren kylpylä",
+      "Ma–to klo 8.00–21.00",
+      "Pe–la klo 8.00–22.00",
+      "Su klo 8.00–20.00",
+      "Pikkulasten allas. Aikuisten altaassa matalia kohtia. Vesihierontapisteitä. Erilaisia saunoja.",
+      "Allasosasto sulkeutuu 30 min ennen kylpylän sulkemista.",
+      "<a href=https://www.scandichotels.fi/hotellit/suomi/jyvaskyla/scandic-laajavuori/kylpyla>Laajavuoren kylpylä</a>"]    
+]
